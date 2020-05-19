6022d13b6d3c99f884789ea1a4fb86b2d967e3ee
AntonKuz357 <kuzminanton@inbox.ru>
2020-05-20 02:45:35 +0300

    Добавил нули перед часами, минутами, секундами, если значение часов, минут, секунд - меньше двух знаков.

--- lesson-1_task-2.py
+++ lesson-1_task-2.py
@@ -4,5 +4,18 @@ hours = user_seconds // 3600
 minutes = (user_seconds % 3600) // 60
 seconds = user_seconds % 60
 
+hours = str(hours)
+minutes = str(minutes)
+seconds = str(seconds)
+
+if len(hours) < 2:
+    hours = '0' + hours
+
+if len(minutes) < 2:
+    minutes = '0' + minutes
+
+if len(seconds) < 2:
+    seconds = '0' + seconds
+
 result = '{}:{}:{}'.format(hours, minutes, seconds)
 print(result)
